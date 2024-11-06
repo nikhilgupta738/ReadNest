48b7710bda75258abbba39d326fb162bd8bcf799
Nikhilbest <ng929439@gmail.com>
2024-11-06 22:16:28 +0530

    add card animation for books

---
+++ frontend/src/components/Cards.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Cards({ item }) {
+    return (
+        <>
+            <div>
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src={item.image}
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Cards
